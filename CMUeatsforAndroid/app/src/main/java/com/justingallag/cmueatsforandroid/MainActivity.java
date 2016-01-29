66d96490ae128ca9d16723630a14726bf897849d
Justin Gallagher <justingallag@gmail.com>
2016-01-29 17:29:11 -0500

    Override activity lifecycle methods

--- CMUeatsforAndroid/app/src/main/java/com/justingallag/cmueatsforandroid/MainActivity.java
+++ CMUeatsforAndroid/app/src/main/java/com/justingallag/cmueatsforandroid/MainActivity.java
@@ -2,12 +2,48 @@ package com.justingallag.cmueatsforandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Run when the app is launched for the first time - initialize your global variables here!
+     * @param savedInstanceState Data saved during the last run (don't worry about this for now!)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Log a message to the console - think of this like System.out.println in typical Java
+        Log.d("MainActivity", "I'm in onCreate!");
+    }
+
+    /**
+     * Run just before your app becomes active, open to user input and able to execute code. This
+     * happens right after onCreate during the first launch, and after the app is brought back into
+     * focus from the background.
+     *
+     * Use this to load saved data, start threads, and open resources you need during execution!
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MainActivity", "I'm in onResume!");
+    }
+
+    /**
+     * Run when your app is suspended to the background or killed entirely - like when the user has
+     * pressed the home button or turned off the screen.
+     *
+     * Use this to close any resources you're using so other apps can access them, and save your
+     * data so it can be reloaded later.
+     */
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "I'm in onPause!");
+
+        super.onPause();
     }
 }
